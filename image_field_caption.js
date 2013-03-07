03f7375830a8e0e2be44b75553242da19d740670
Casey Grzecka <casey.grzecka@unlv.edu>
2013-03-07 12:49:37 -0800

    attempt at persistent captions for multi-image fields

--- image_field_caption.js
+++ image_field_caption.js
@@ -1,60 +1,52 @@
 (function ($) {
   Drupal.behaviors.imageFieldCaption = {
     attach: function (context, settings) {
+      var cap = {};
       // remove localStorage on fresh load
-      // count number of images
-      var image_count = $('table > tbody > tr').length;
-      if (image_count == 0) {
-        // remove all localStorage
-        for (var i = 0; i < localStorage.length; i++) {
-          localStorage.removeItem(i);
+      // get number of images
+      $.each($('table'), function() {
+        if ($(this).attr('id').length > 0) {
+          cap.field_name = $(this).attr('id').substring(0, $(this).attr('id').length - 10);
         }
-      } else if (image_count > 0) {
+      });
+      cap.count = $('table > tbody > tr').length - 6;
+      if (cap.count <= 0) {
+        // remove all localStorage
+        var i = 0;
+        while (localStorage.length >= 1) {
+          localStorage.removeItem(i);
+          i++;
+        }
+      } else if (cap.count > 0) {
         for (var i = 0; i < localStorage.length; i++){
           var item = localStorage.getItem(i);
-          $('#edit-field-image-und-'+ i +'-caption').val(item);
+          $('#'+ cap.field_name +'-und-'+ i +'-caption').val(item);
         } 
       }
       // save captions in localStorage
-      $('div.field-type-image input.form-submit').on('mousedown',function(e) {
-        var button_type = e.currentTarget.id.substring(e.currentTarget.id.length - 13);
-//        console.log(button_type);i
-        
-        // get the caption id
-        var capids = $(this).attr('id').replace(/\D/g,'');
-
-        if (button_type == 'upload-button') {
-          var capids_m1 = capids-1;
-          // set the value of the captions
-          localStorage.setItem(capids_m1,  $('#edit-field-image-und-'+ capids_m1 +'-caption').val());
+      $('div.field-type-image textarea').blur(function(e) {
+        cap.id = $(this).attr('id').replace(/\D/g,'');
+        cap.text = $('#'+ $(this).attr('id')).val();
+        localStorage.setItem(cap.id, cap.text);
+      });
+      // event for removing caption from removed image
+      $('div.field-type-image input.form-submit').mousedown(function(e) {
+        var button = {};
+        button.type = e.currentTarget.id.substring(e.currentTarget.id.length - 13);
+        if (button.type == 'remove-button') {
+          // get the button id
+          button.id = $(this).attr('id').replace(/\D/g,'');
+          cap.count_m1 = cap.count - 1;
+          // if last item, just delete it
+          if (button.id == cap.count_m1) {
+            localStorage.removeItem(button.id);
+          } else {
+            //TODO: remove other items than the last one
+            localStorage.removeItem(button.id);
+            alert("Sorry, you may have to manually fix your captions. We're working on this!");
+          }
         }
-        //TODO: add remove button case
-        if (button_type == 'remove-button') {
-          // remove that row from localStorage on mousedown
-          //localStorage.removeItem(capids);
-          // re-arrange the keys to match the new set of images 
-          //by re-saving all values of keys higher than the removed key with the value of key +1
-//          for (capids; capids < localStorage.length; capids++) {
-//            if...
-//            var capids_p1 = capids + 1;
-//            console.log(capids_p1);
-//            var capids_p1_val = localStorage.getItem(capids_p1);
-//            console.log(capids_p1_val);
-//          }
-          // remove the last item
-
-        }
-
-        //TODO: add re-order case (may just work, needs more testing)
       });
     }
   };
-
-  $(document).ajaxComplete(function(e) {
-    // set the value of the captions from localStorage
-    for (var i = 0; i < localStorage.length; i++){
-      var item = localStorage.getItem(i);
-      $('#edit-field-image-und-'+ i +'-caption').val(item);
-    }
-  });
 })(jQuery);
